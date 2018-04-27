90be927c3bab5ee2b1c7483568760c0c29d72a6d
Razvan <Razvan@DESKTOP-L6TDLJO>
2018-04-27 22:22:49 +0300

    Refactored

--- src/main/java/primerange/PrimeRange.java
+++ src/main/java/primerange/PrimeRange.java
@@ -3,40 +3,49 @@ package primerange;
 import java.math.BigInteger;
 import java.util.Random;
 
-public class PrimeRange
-{
+/**
+ A class that wraps up a few methods for determining
+ the smallest, largest prime number from an interval. The noIterations
+ static member is used by nondeterministic algorithms like Miller-Rabin and Fermat.
+ */
+public class PrimeRange {
 
     public static final long INVALID_PRIME = -1;
 
-    public enum Strategy
-    {
-        CLASSIC_DETERMINISTIC, MILLER_RABIN
+    public static int noIterations = 5;
+
+    public enum Strategy {
+        DETERMINISTIC, NON_DETERMINISTIC
     }
 
     /**
-     * Extract a positive prime from the given range
-     * @param lowerLimit the lower limit for searching for the prime > 0
-     * @param upperLimit the upper limit for searching for the prime > 0
-     * @param currentStrategy the strategy employed (A/B/C/D)
-     * @return
+     Extract the largest prime from the given range (from A->B V B->A depending on the largest value)
+     @param limitA          the first limit for searching for the prime
+     @param limitB          the second limit for searching for the prime
+     @param currentStrategy the strategy employed (DETERMINISTIC/NON_DETERMINISTIC)
+     @return the biggest prime number or INVALID_PRIME if no prime has been found
      */
-    public static long extractLargestPrime(long lowerLimit, long upperLimit, Strategy currentStrategy)
-    {
+    public static long extractLargestPrime(long limitA, long limitB, Strategy currentStrategy) {
+
+        if (limitA > limitB) {
+            limitA = limitA ^ limitB;
+            limitB = limitA ^ limitB;
+            limitA = limitA ^ limitB;
+        }
+
         int nbIterations = 5;
-        for (long i = upperLimit; i >= lowerLimit; --i)
-        {
-            switch (currentStrategy)
-            {
-                case CLASSIC_DETERMINISTIC:
-                default:
-                {
-                    if (isPrimeV2(i))
+        for (long i = limitB; i >= limitA; --i) {
+            switch (currentStrategy) {
+                case DETERMINISTIC:
+                default: {             /* Classic optimized */
+                    if (isPrimeV2(i)) {
                         return i;
+                    }
                 }
-                case MILLER_RABIN:
-                {
-                    if (isPrimeV3(i, nbIterations))
+                case NON_DETERMINISTIC: { /* Miller Rabin */
+                    if (isPrimeV3(i)) {
                         return i;
+                    }
                 }
             }
         }
@@ -45,30 +54,34 @@ public class PrimeRange
     }
 
     /**
-     * Extract a positive prime from the given range
-     * @param lowerLimit the lower limit for searching for the prime > 0
-     * @param upperLimit the upper limit for searching for the prime > 0
-     * @param currentStrategy the strategy employed (A/B/C/D)
-     * @return
+     Extract the smallest prime from the given range (from A->B V B->A depending on the largest value)
+     @param limitA          the first limit for searching for the prime
+     @param limitB          the second limit for searching for the prime
+     @param currentStrategy the strategy employed (DETERMINISTIC/NON_DETERMINISTIC)
+     @return the smallest prime number or INVALID_PRIME if no prime has been found
      */
-    public static long extractSmallestPrime(long lowerLimit, long upperLimit, Strategy currentStrategy)
-    {
+    public static long extractSmallestPrime(long limitA, long limitB, Strategy currentStrategy) {
+
+        if (limitA > limitB) {
+            limitA = limitA ^ limitB;
+            limitB = limitA ^ limitB;
+            limitA = limitA ^ limitB;
+        }
+
         int nbIterations = 5;
 
-        for (long i = lowerLimit; i <= upperLimit; ++i)
-        {
-            switch (currentStrategy)
-            {
-                case CLASSIC_DETERMINISTIC:
-                default:
-                {
-                    if (isPrimeV2(i))
+        for (long i = limitA; i <= limitB; ++i) {
+            switch (currentStrategy) {
+                case DETERMINISTIC:
+                default: {             /* Classic optimized */
+                    if (isPrimeV2(i)) {
                         return i;
+                    }
                 }
-                case MILLER_RABIN:
-                {
-                    if (isPrimeV3(i, nbIterations))
+                case NON_DETERMINISTIC: { /* Miller Rabin */
+                    if (isPrimeV3(i)) {
                         return i;
+                    }
                 }
             }
         }
@@ -76,49 +89,54 @@ public class PrimeRange
         return INVALID_PRIME;
     }
 
-
     /**
-     * Classic primality test
-     *
-     * @param numberTested
-     * @return
+     Classic primality test
+     @param numberTested
+     @return true or false
      */
-    private static boolean isPrimeV1(long numberTested)
-    {
-        if (numberTested <= 1)
+    protected static boolean isPrimeV1(long numberTested) {
+
+        numberTested = Math.abs(numberTested);
+
+        if (numberTested <= 1) {
             return false;
+        }
 
         long limit = (long) Math.sqrt(numberTested);
-        for (long j = 2; j <= limit; ++j)
-        {
-            if (numberTested % j == 0)
+        for (long j = 2; j <= limit; ++j) {
+            if (numberTested % j == 0) {
                 return false;
+            }
         }
 
         return true;
     }
 
     /**
-     * Classic primality test optimized
-     *
-     * @param numberTested
-     * @return
+     Classic primality test optimized
+     @param numberTested
+     @return true or false
      */
-    private static boolean isPrimeV2(long numberTested)
-    {
-        if (numberTested <= 1)
+    protected static boolean isPrimeV2(long numberTested) {
+
+        numberTested = Math.abs(numberTested);
+
+        if (numberTested <= 1) {
             return false;
-        else if (numberTested <= 3)
+        }
+        else if (numberTested <= 3) {
             return true;
-        else if (numberTested % 2 == 0 || numberTested % 3 == 0)
+        }
+        else if (numberTested % 2 == 0 || numberTested % 3 == 0) {
             return false;
+        }
 
         long j = 5;
         long limit = (long) Math.sqrt(numberTested);
-        while (j <= limit)
-        {
-            if (numberTested % j == 0 || numberTested % (j + 2) == 0)
+        while (j <= limit) {
+            if (numberTested % j == 0 || numberTested % (j + 2) == 0) {
                 return false;
+            }
             j += 6;
         }
 
@@ -126,85 +144,97 @@ public class PrimeRange
     }
 
     /**
-     * Miller-Rabine primality test (non-deterministic)
-     * @param numberTested
-     * @param noIterations
-     * @return
+     Miller-Rabine primality test (non-deterministic)
+     @param numberTested
+     @return true or false
      */
-    private static boolean isPrimeV3(long numberTested, int noIterations)
-    {
+    protected static boolean isPrimeV3(long numberTested) {
+
+        if (noIterations <= 0)
+            return false;
+
+        numberTested = Math.abs(numberTested);
+
         /** base case **/
-        if (numberTested == 0 || numberTested == 1)
+        if (numberTested == 0 || numberTested == 1) {
             return false;
+        }
         /** base case - 2 is prime **/
-        if (numberTested == 2)
+        if (numberTested == 2) {
             return true;
+        }
         /** an even number other than 2 is composite **/
-        if (numberTested % 2 == 0)
+        if (numberTested % 2 == 0) {
             return false;
+        }
 
         long s = numberTested - 1;
-        while (s % 2 == 0)
+        while (s % 2 == 0) {
             s /= 2;
+        }
 
         Random randGen = new Random();
-        for (int i = 0; i < noIterations; i++)
-        {
+        for (int i = 0; i < noIterations; i++) {
             long randomVal = Math.abs(randGen.nextLong());
             long a = randomVal % (numberTested - 1) + 1, temp = s;
             long mod = modularExponentiation(a, temp, numberTested);
-            while (temp != numberTested - 1 && mod != 1 && mod != numberTested - 1)
-            {
+            while (temp != numberTested - 1 && mod != 1 && mod != numberTested - 1) {
                 mod = modularMultiplication(mod, mod, numberTested);
                 temp *= 2;
             }
-            if (mod != numberTested - 1 && temp % 2 == 0)
+            if (mod != numberTested - 1 && temp % 2 == 0) {
                 return false;
+            }
         }
         return true;
     }
 
     /**
-     * Fermat primality test (non-deterministic)
-     * @param numberTested
-     * @param noIterations
-     * @return
+     Fermat primality test (non-deterministic)
+     @param numberTested
+     @return true or false
      */
-    public static boolean isPrimeV4(long numberTested, int noIterations)
-    {
+    protected static boolean isPrimeV4(long numberTested) {
+
+        if (noIterations <= 0)
+            return false;
+
+        numberTested = Math.abs(numberTested);
+
         /** base case **/
-        if (numberTested == 0 || numberTested == 1)
+        if (numberTested == 0 || numberTested == 1) {
             return false;
+        }
         /** base case - 2 is prime **/
-        if (numberTested == 2)
+        if (numberTested == 2) {
             return true;
+        }
         /** an even number other than 2 is composite **/
-        if (numberTested % 2 == 0)
+        if (numberTested % 2 == 0) {
             return false;
+        }
 
         Random rand = new Random();
-        for (int i = 0; i < noIterations; i++)
-        {
+        for (int i = 0; i < noIterations; i++) {
             long r = Math.abs(rand.nextLong());
             long a = r % (numberTested - 1) + 1;
-            if (modularExponentiation(a, numberTested - 1, numberTested) != 1)
+            if (modularExponentiation(a, numberTested - 1, numberTested) != 1) {
                 return false;
+            }
         }
         return true;
     }
 
     /**
-     * Helper method that computes (a ^ b) % c
-     * @param a
-     * @param b
-     * @param c
-     * @return
+     Helper method that computes (a ^ b) % c
+     @param a
+     @param b
+     @param c
+     @return (a ^ b) % c
      */
-    private static long modularExponentiation(long a, long b, long c)
-    {
+    protected static long modularExponentiation(long a, long b, long c) {
         long result = 1;
-        for (int i = 0; i < b; i++)
-        {
+        for (long i = 0; i < b; i++) {
             result *= a;
             result %= c;            // prevents overflowing long
         }
@@ -212,34 +242,33 @@ public class PrimeRange
     }
 
     /**
-     * Helper method that computes (a * b) % c
-     * @param a
-     * @param b
-     * @param c
-     * @return
+     Helper method that computes (a * b) % c
+     @param a
+     @param b
+     @param c
+     @return (a * b) % c
      */
-    private static long modularMultiplication(long a, long b, long c)
-    {
+    protected static long modularMultiplication(long a, long b, long c) {
         return BigInteger.valueOf(a).multiply(BigInteger.valueOf(b)).mod(BigInteger.valueOf(c)).longValue();
     }
 
-    public static void main(String[] argc)
-    {
-        System.out.println("Hello world");
-        System.out.println(extractPrime(2, 11, Strategy.APPROACH_A, Criteria.SMALLEST));
-        System.out.println(extractPrime(2, 11, Strategy.APPROACH_B, Criteria.SMALLEST));
-        System.out.println(extractPrime(2, 11, Strategy.APPROACH_C, Criteria.SMALLEST));
-        System.out.println(extractPrime(2, 11, Strategy.APPROACH_D, Criteria.SMALLEST));
-        System.out.println(extractPrime(150, 300, Strategy.APPROACH_A, Criteria.GREATEST));
-        System.out.println(extractPrime(150, 300, Strategy.APPROACH_B, Criteria.GREATEST));
-        System.out.println(extractPrime(150, 300, Strategy.APPROACH_C, Criteria.GREATEST));
-        System.out.println(extractPrime(150, 300, Strategy.APPROACH_D, Criteria.GREATEST));
+    public static void main(String[] argc) {
+        System.out.println("Largest prime -> deterministic");
+        System.out.println(extractLargestPrime(2, 11, Strategy.DETERMINISTIC));
+        System.out.println(extractLargestPrime(150, 300, Strategy.DETERMINISTIC));
+        System.out.println("Largest prime -> nondeterministic");
+        System.out.println(extractLargestPrime(2, 11, Strategy.NON_DETERMINISTIC));
+        System.out.println(extractLargestPrime(150, 300, Strategy.NON_DETERMINISTIC));
+
+        System.out.println("Smallest prime -> deterministic");
+        System.out.println(extractSmallestPrime(2, 11, Strategy.DETERMINISTIC));
+        System.out.println(extractSmallestPrime(150, 300, Strategy.DETERMINISTIC));
+        System.out.println("Smallest prime -> nondeterministic");
+        System.out.println(extractSmallestPrime(2, 11, Strategy.NON_DETERMINISTIC));
+        System.out.println(extractSmallestPrime(150, 300, Strategy.NON_DETERMINISTIC));
+
+        System.out.println("Smallest prime -> deterministic");
+        System.out.println(extractSmallestPrime(-5, 11, Strategy.DETERMINISTIC));
     }
-
-
-    private long lowerLimitRange;
-    private long upperLimitRange;
-    private Strategy strategyUsed;
-    private Criteria primeCriteria;
 }
 
--- src/test/java/primerange/PrimeRangeTest.java
+++ src/test/java/primerange/PrimeRangeTest.java
@@ -1,19 +1,153 @@
 package primerange;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
 
-public class PrimeRangeTest
-{
+/**
+ Class with tests for the PrimeRange class
+ */
+public class PrimeRangeTest {
+    /**
+     Equivalence partitioning tests
+     @throws Exception
+     */
+    @org.junit.Test
+    public void equivalencePartitioning() throws Exception {
 
-    @Test
-    public void equivalencePartitioning()
-    {
-        assertEquals(2, PrimeRange.extractPrime(2, 11, PrimeRange.Strategy.APPROACH_A, PrimeRange.Criteria.SMALLEST));
-        assertEquals(2, PrimeRange.extractPrime(2, 11, PrimeRange.Strategy.APPROACH_B, PrimeRange.Criteria.SMALLEST));
+        /*
+        Boundary parameters equivalence classes -> order 3 tuple (0, a, b) -> 3! = 6 classes
+        C_1 = { (a, b) in ZxZ / 0 < a < b }
+        C_2 = { (a, b) in ZxZ / a < 0 < b }
+        C_3 = { (a, b) in ZxZ / 0 < b < a }
+        C_4 = { (a, b) in ZxZ / b < a < 0 }
+        C_5 = { (a, b) in ZxZ / a < b < 0 }
+        C_6 = { (a, b) in ZxZ / b < 0 < a }
 
-        assertEquals(11, PrimeRange.extractPrime(2, 11, PrimeRange.Strategy.APPROACH_A, PrimeRange.Criteria.GREATEST));
-        assertEquals(11, PrimeRange.extractPrime(2, 11, PrimeRange.Strategy.APPROACH_B, PrimeRange.Criteria.GREATEST));
+        Strategy parameter equivalence classes -> 2 classes
+        M_1 = { m / m = DETERMINISTIC }
+        M_2 = { m / m = NON_DETERMINISTIC }
+
+        Return value equivalence classes -> 2 classes
+        R_1 = { r / r != INVALID_PRIME }
+        R_2 = { r / r  = INVALID_PRIME }
+
+        In total -> |T| = 6 * 2 * 2 = 24 independent tests
+        */
+
+        // 1-1-1                                    a   b
+        assertEquals(PrimeRange.extractLargestPrime(2, 11, PrimeRange.Strategy.DETERMINISTIC), 11);
+        // 1-1-2
+        assertEquals(PrimeRange.extractLargestPrime(14, 16, PrimeRange.Strategy.DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 1-2-1
+        assertEquals(PrimeRange.extractLargestPrime(2, 11, PrimeRange.Strategy.NON_DETERMINISTIC), 11);
+        // 1-2-2
+        assertEquals(PrimeRange.extractLargestPrime(14, 16, PrimeRange.Strategy.NON_DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 2-1-1
+        assertEquals(PrimeRange.extractLargestPrime(-2, 11, PrimeRange.Strategy.DETERMINISTIC), 11);
+        // 2-1-2
+        assertEquals(PrimeRange.extractLargestPrime(-1, 1, PrimeRange.Strategy.DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 2-2-1
+        assertEquals(PrimeRange.extractLargestPrime(-1, 2, PrimeRange.Strategy.NON_DETERMINISTIC), 2);
+        // 2-2-2
+        assertEquals(PrimeRange.extractLargestPrime(-1, 1, PrimeRange.Strategy.NON_DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 3-1-1
+        assertEquals(PrimeRange.extractLargestPrime(11, 2, PrimeRange.Strategy.DETERMINISTIC), 11);
+        // 3-1-2
+        assertEquals(PrimeRange.extractLargestPrime(16, 14, PrimeRange.Strategy.DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 3-2-1
+        assertEquals(PrimeRange.extractLargestPrime(11, 2, PrimeRange.Strategy.NON_DETERMINISTIC), 11);
+        // 3-2-2
+        assertEquals(PrimeRange.extractLargestPrime(16, 14, PrimeRange.Strategy.NON_DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 4-1-1
+        assertEquals(PrimeRange.extractLargestPrime(-1, -2, PrimeRange.Strategy.DETERMINISTIC), -2);
+        // 4-1-2
+        assertEquals(PrimeRange.extractLargestPrime(-15, -16, PrimeRange.Strategy.DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 4-2-1
+        assertEquals(PrimeRange.extractLargestPrime(-1, -2, PrimeRange.Strategy.NON_DETERMINISTIC), -2);
+        // 4-2-2
+        assertEquals(PrimeRange.extractLargestPrime(-15, -16, PrimeRange.Strategy.NON_DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 5-1-1
+        assertEquals(PrimeRange.extractLargestPrime(-2, -1, PrimeRange.Strategy.DETERMINISTIC), -2);
+        // 5-1-2
+        assertEquals(PrimeRange.extractLargestPrime(-16, -15, PrimeRange.Strategy.DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 5-2-1
+        assertEquals(PrimeRange.extractLargestPrime(-2, -1, PrimeRange.Strategy.NON_DETERMINISTIC), -2);
+        // 5-2-2
+        assertEquals(PrimeRange.extractLargestPrime(-16, -15, PrimeRange.Strategy.NON_DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 6-1-1
+        assertEquals(PrimeRange.extractLargestPrime(11, -2, PrimeRange.Strategy.DETERMINISTIC), 11);
+        // 6-1-2
+        assertEquals(PrimeRange.extractLargestPrime(1, -1, PrimeRange.Strategy.DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 6-2-1
+        assertEquals(PrimeRange.extractLargestPrime(2, -1, PrimeRange.Strategy.NON_DETERMINISTIC), 2);
+        // 6-2-2
+        assertEquals(PrimeRange.extractLargestPrime(1, -1, PrimeRange.Strategy.NON_DETERMINISTIC), PrimeRange.INVALID_PRIME);
+    }
+
+    /**
+     Boundary analysis tests
+     @throws Exception
+     */
+    @org.junit.Test
+    public void boundaryAnalysis() throws Exception {
+
+        /*
+        Equivalence classes for the boundaries - The boundary is actually when l = u
+        B_1 -> { (a, b) / a = b , a < 0}
+        B_2 -> { (a, b) / a = b , a = 0}
+        B_3 -> { (a, b) / a = b , a > 0}
+
+        In total, by combining with M_1, M_2, R_1, R_2 -> |T| = 10 tests
+        */
+
+        // 1-1-1
+        assertEquals(PrimeRange.extractLargestPrime(-17, -17, PrimeRange.Strategy.DETERMINISTIC), -17);
+        // 1-1-2
+        assertEquals(PrimeRange.extractLargestPrime(-1, -1,   PrimeRange.Strategy.DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 1-2-1
+        assertEquals(PrimeRange.extractLargestPrime(-17, -17, PrimeRange.Strategy.NON_DETERMINISTIC), -17);
+        // 1-2-2
+        assertEquals(PrimeRange.extractLargestPrime(-1, -1,   PrimeRange.Strategy.NON_DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 2-1-2
+        assertEquals(PrimeRange.extractLargestPrime(0, 0, PrimeRange.Strategy.DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 2-2-2
+        assertEquals(PrimeRange.extractLargestPrime(0, 0, PrimeRange.Strategy.NON_DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 3-1-1
+        assertEquals(PrimeRange.extractLargestPrime(23, 23, PrimeRange.Strategy.DETERMINISTIC), 23);
+        // 3-1-2
+        assertEquals(PrimeRange.extractLargestPrime(200, 200, PrimeRange.Strategy.DETERMINISTIC), PrimeRange.INVALID_PRIME);
+        // 3-2-1
+        assertEquals(PrimeRange.extractLargestPrime(23, 23, PrimeRange.Strategy.NON_DETERMINISTIC), 23);
+        // 3-2-2
+        assertEquals(PrimeRange.extractLargestPrime(200, 200, PrimeRange.Strategy.NON_DETERMINISTIC), PrimeRange.INVALID_PRIME);
+    }
+
+    /**
+     Category partitioning tests
+     @throws Exception
+     */
+    @org.junit.Test
+    public void categoryPartitioning() throws Exception {
+
+        // Test isPrimeV2(x)
+        // x > 0
+        assertTrue(PrimeRange.isPrimeV2(15485863));
+        // x = 1
+        assertFalse(PrimeRange.isPrimeV2(1));
+        // x = 0
+        assertFalse(PrimeRange.isPrimeV2(0));
+        // x < 0
+        assertTrue(PrimeRange.isPrimeV2(-15485863));
+
+        // Test isPrimeV4(x)
+        // x > 0
+        assertTrue(PrimeRange.isPrimeV4(15485863));
+        // x =1
+        assertFalse(PrimeRange.isPrimeV4(1));
+        // x = 0
+        assertFalse(PrimeRange.isPrimeV4(0));
+        // x < 0
+        assertTrue(PrimeRange.isPrimeV4(-15485863));
+
+        //
     }
 }
